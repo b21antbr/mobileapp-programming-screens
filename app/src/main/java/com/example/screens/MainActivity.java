42f65d1233415dc4760bd3dfc9fd8320bf4cbe36
Anton Brännholm <b21antbr@student.his.se>
2022-04-26 14:52:55 +0200

    added second activity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -12,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("name", "Anton");
+        startActivity(intent);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+            // Do something with the name and number
+        }
     }
 }
